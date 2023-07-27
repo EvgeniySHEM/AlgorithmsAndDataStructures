f527b4766b7b36450ba92fe890b2a71aaf39c230
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-07-27 21:34:25 +0300

    рекурсивно рекурсивно c кешем

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,21 +1,55 @@
-
-import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Fibonacci {
+    static Map<Integer, BigInteger> cash = new HashMap<>();
+
     public static void main(String[] args) {
-        System.out.println(fibEffective(841645));
+
+        System.out.println(fibEffective(300));
+//        System.out.println(recFib(45));
+        System.out.println(recFibWithCash(300));
     }
 
-    private static long fibEffective(int n) {
-        long[] array = new long[n + 1];
+    private static BigInteger fibEffective(int n) {
+        BigInteger[] array = new BigInteger[n + 1];
 
-        array[0] = 0;
-        array[1] = 1;
+        array[0] = BigInteger.valueOf(0);
+        array[1] = BigInteger.valueOf(1);
 
         for (int i = 2; i <= n; i++) {
-            array[i] = array[i - 1] + array[i - 2];
+            array[i] = array[i - 1].add(array[i - 2]);
         }
 
         return array[n];
+    }
+
+    /**
+     * рекурсивно
+     */
+    private static int recFib(int i) {
+        if (i < 2) {
+            return i;
+        }
+        return recFib(i - 1) + recFib(i - 2);
+    }
+
+    /**
+     * рекурсивно c кешем
+     */
+
+    private static BigInteger recFibWithCash(int i) {
+        if (i < 2) {
+            return BigInteger.valueOf(i);
+        }
+
+        if (cash.containsKey(i)) {
+            return cash.get(i);
+        } else {
+            BigInteger res = recFibWithCash(i - 1).add(recFibWithCash(i - 2));
+            cash.put(i, res);
+            return res;
+        }
     }
 }
