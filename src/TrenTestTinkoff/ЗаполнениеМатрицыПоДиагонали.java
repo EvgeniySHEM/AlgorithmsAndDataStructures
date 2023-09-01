624e1695e3841d5345c3e9b73b07f1e3dd515a6e
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-09-01 20:46:06 +0300

    Самый длинный общий префикс

--- src/TrenTestTinkoff/ЗаполнениеМатрицыПоДиагонали.java
+++ src/TrenTestTinkoff/ЗаполнениеМатрицыПоДиагонали.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ЗаполнениеМатрицыПоДиагонали {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] matrix = new int[m][n];
@@ -12,6 +13,7 @@ public class ЗаполнениеМатрицыПоДиагонали {
         int b = 0;
         int counter = 1;
         boolean flag = false;
+
         for (int i = 0; i < n + m - 1; i++) {// проходим по всем диагоналям
             a++;
             if(flag) {
