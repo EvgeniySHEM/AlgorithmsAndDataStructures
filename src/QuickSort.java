55b5acb3b24fcf4fde535fa3f2a5ae9c865c526e
Evgeniy Sharychenkov <e.sharychenkov@astondevs.ru>
2024-04-02 20:15:54 +0300

    refactor

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -3,10 +3,11 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class QuickSort {
-    private static Random random = ThreadLocalRandom.current();
+    private static final Random random = ThreadLocalRandom.current();
+
     public static void main(String[] args) {
 
-        int[] sortArr = {12, 6, 4, 1, 15, 1, 18, 32,-3, 0, 10};
+        int[] sortArr = {2, 1, 10, 5, 6, 2, 3, 2, 15, 83, 42, 25, 50, 31, 0, -5, -10, 18};
         quickSort(sortArr, 0, sortArr.length - 1);
         System.out.println(Arrays.toString(sortArr));
 
@@ -18,7 +19,7 @@ public class QuickSort {
 
         //выбираем опорный элемент
 //        int middle = low + (high - low) / 2;
-        int middle = random.nextInt(low,high + 1);
+        int middle = random.nextInt(low, high + 1);
         int border = sortArr[middle];
 
         //разделияем на подмассивы и меняем местами
@@ -26,18 +27,18 @@ public class QuickSort {
         while (i <= j) {
             while (sortArr[i] < border) i++;
             while (sortArr[j] > border) j--;
-            if (i <= j) {
-                int swap = sortArr[i];
-                sortArr[i] = sortArr[j];
-                sortArr[j] = swap;
-                i++;
-                j--;
-            }
+            if (i <= j) swap(sortArr, i++, j--);
         }
 
         //рекурсия для сортировки левой и правой части
         if (low < j) quickSort(sortArr, low, j);
         if (high > i) quickSort(sortArr, i, high);
+    }
+
+    private static void swap(int[] sortArr, int i, int j) {
+        int swap = sortArr[i];
+        sortArr[i] = sortArr[j];
+        sortArr[j] = swap;
     }
 }
 
